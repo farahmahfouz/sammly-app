a5e8b4bc273981314c459fd7f121321a6b51ab99
farahmahfouz <farahmahfouz11@gmail.com>
2026-09-19 20:19:58 +0300

    refact: catefory section

---
+++ src/components/Landing/CategoryCard.jsx
@@ -0,0 +1,34 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom"
+import ArrowRight from "../../icons/ArrowRight"
+
+function CategoryCard({ cat }) {
+    return (
+        <Link
+            to="/products"
+            key={cat.id}
+            className=" rounded-lg overflow-hidden group cursor-pointer block"
+        >
+            <img
+                src={cat.image}
+                alt={cat.name}
+                className="w-full h-60 object-cover group-hover:scale-105 transition-transform duration-300"
+            />
+            <div className="bg-surface border border-borderLight p-4">
+                <div>
+                    <div className='flex items-center justify-between text-primary hover:text-primaryDark'>
+
+                        <h3 className="text-base font-semibold capitalize text-textPrimary">{cat.name}</h3>
+                        <span className='border border-borderLight p-1 rounded-full'>
+                            <ArrowRight />
+                        </span>
+                    </div>
+                    <p className="text-sm tracking-tighter text-textSecondary">{cat.description}</p>
+                </div>
+
+            </div>
+        </Link>
+    )
+}
+
+export default CategoryCard
--- src/components/Landing/CategorySection.jsx
+++ src/components/Landing/CategorySection.jsx
@@ -1,6 +1,7 @@
 import Explore from './../../icons/Explore';
 import { Link } from "react-router-dom";
 import ArrowRight from "../../icons/ArrowRight";
+import CategoryCard from './CategoryCard';
 
 
 const categories = [
@@ -63,30 +64,7 @@ function CategorySection() {
 
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-4">
         {categories.map((cat) => (
-          <Link
-            to="/products"
-            key={cat.id}
-            className=" rounded-lg overflow-hidden group cursor-pointer block"
-          >
-            <img
-              src={cat.image}
-              alt={cat.name}
-              className="w-full h-60 object-cover group-hover:scale-105 transition-transform duration-300"
-            />
-            <div className="bg-surface border border-borderLight p-4">
-              <div>
-                <div className='flex items-center justify-between text-primary hover:text-primaryDark'>
-
-                  <h3 className="text-base font-semibold capitalize text-textPrimary">{cat.name}</h3>
-                  <span className='border border-borderLight p-1 rounded-full'>
-                    <ArrowRight />
-                  </span>
-                </div>
-                <p className="text-sm tracking-tighter text-textSecondary">{cat.description}</p>
-              </div>
-
-            </div>
-          </Link>
+          <CategoryCard cat={cat} key={cat.id} />
         ))}
       </div>
     </section>
