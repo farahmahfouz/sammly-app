2ef57fdf7646e6804360e2f99cf0f08a4d61f497
farahmahfouz <farahmahfouz11@gmail.com>
2026-09-19 19:42:46 +0300

    style: change categories section style

--- src/components/Landing/CategorySection.jsx
+++ src/components/Landing/CategorySection.jsx
@@ -1,28 +1,93 @@
-import KidsCategorySection from "./CategoriesSection/KidsCategorySection";
-import MenCategorySection from "./CategoriesSection/MenCategorySection";
-import WomanCatregorySection from "./CategoriesSection/WomanCatregorySection";
+import Explore from './../../icons/Explore';
+import { Link } from "react-router-dom";
+import ArrowRight from "../../icons/ArrowRight";
+
+
+const categories = [
+  {
+    id: 1,
+    name: "Men",
+    description: "Classic & Trendy",
+    image: "/categories/man-style.png",
+  },
+  {
+    id: 2,
+    name: "Women",
+    description: "Stylish & Comfy",
+    image: "/categories/women-style.png",
+  },
+  {
+    id: 3,
+    name: "Kids",
+    description: "Fun & Colorful",
+    image: "/categories/kids-style.png",
+  },
+  {
+    id: 4,
+    name: "Hoodies",
+    description: "Warm & Cozy",
+    image: "/categories/hoodies-style.png",
+  },
+  {
+    id: 5,
+    name: "Shoes",
+    description: "Everyday Comfort",
+    image: "/categories/print-shoes.jpg",
+  },
+  {
+    id: 6,
+    name: "Accessories",
+    description: "Mugs, Bags & More",
+    image: "/categories/accessories.jpg",
+  },
+];
+
+
 function CategorySection() {
+
   return (
-    <section className="py-32 ">
-      <div className="text-center mb-6">
-        <h2 className="text-2xl font-bold text-textColor">CATEGORY SECTION</h2>
-        <p className="text-gray-500 mb-11">
-          Explore our diverse categories to find the perfect styles <br /> that
-          suit your taste and needs
-        </p>
+    <section className="py-28 container mx-auto">
+      <div className="text-start mb-6">
+        <span className="flex gap-1 text-primary items-center">
+          <Explore />
+          <h2 className="text-xs font-semibold uppercase tracking-tighter">explore our collection</h2>
+        </span>
+        <div className="flex justify-between">
+          <p className="capitalize text-3xl font-extrabold text-textPrimary">shop by style</p>
+          <Link to="/products" className="flex gap-2 items-center capitalize text-sm font-medium tracking-tight text-primary hover:text-primaryDark">
+            view all styles <ArrowRight />
+          </Link>
+        </div>
+        <p className="first-letter:capitalize text-textMuted text-sm">find the perfect fit for your vibe.</p>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6 p-3 pt-32 lg:mx-28">
-        {/* Men Section */}
-        <MenCategorySection></MenCategorySection>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-6 gap-4">
+        {categories.map((cat) => (
+          <Link
+            to="/products"
+            key={cat.id}
+            className=" rounded-lg overflow-hidden group cursor-pointer block"
+          >
+            <img
+              src={cat.image}
+              alt={cat.name}
+              className="w-full h-60 object-cover group-hover:scale-105 transition-transform duration-300"
+            />
+            <div className="bg-surface border border-borderLight p-4">
+              <div>
+                <div className='flex items-center justify-between text-primary hover:text-primaryDark'>
 
-        {/* Kids Section */}
-        <KidsCategorySection></KidsCategorySection>
+                  <h3 className="text-base font-semibold capitalize text-textPrimary">{cat.name}</h3>
+                  <span className='border border-borderLight p-1 rounded-full'>
+                    <ArrowRight />
+                  </span>
+                </div>
+                <p className="text-sm tracking-tighter text-textSecondary">{cat.description}</p>
+              </div>
 
-        {/* Women's Section */}
-        <div className="relative">
-          <WomanCatregorySection></WomanCatregorySection>
-        </div>
+            </div>
+          </Link>
+        ))}
       </div>
     </section>
   );
